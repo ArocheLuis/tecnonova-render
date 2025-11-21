f49018948957be974a47b758421412d5c8e4e062
ArocheLuis <larochem@miumg.edu.gt>
2025-11-20 23:08:02 -0600

    cors5

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
+app.options('*', cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
