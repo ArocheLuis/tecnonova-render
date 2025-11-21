e14aa274de7150518ef01403abdb4c06e30b382e
ArocheLuis <larochem@miumg.edu.gt>
2025-11-20 23:04:43 -0600

    cors4

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app=express();
 app.use(cors({
   origin: [
     'http://localhost:5173',           
-    'https://tecnonova-render.onrender.com' 
+    'https://tecnonova-render.onrender.com/api/auth/register' 
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
